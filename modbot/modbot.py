31d94c881c16dee132acede8e01801e3ed5faa2e
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-10 21:53:29 -0700

    Process inputs

--- modbot/modbot.py
+++ modbot/modbot.py
@@ -5,6 +5,23 @@ from dotbot.dispatcher import Dispatcher
 
 
 def add(config_file, filename, target=None):
+    path, filename = os.path.split(filename)
+
+    if not path:
+        path = os.getcwd()
+
+    if target is None:
+        target = filename
+
+    target_path, target = os.path.split(target)
+    config_path, config_file = os.path.split(config_file)
+
+    if not target_path:
+        target_path = os.getenv('DOTFILES')
+
+    if not config_path:
+        config_path = os.getenv('DOTFILES')
+
     config = _read_config(config_file)
     i = [i for i, d in enumerate(config) if 'link' in d][0]
     config[i]['link'][filename] = target if target else filename
