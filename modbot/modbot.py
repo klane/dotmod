35775a2c11ba317c52eec48eecc9355f81169136
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-10 22:14:01 -0700

    Print status message

--- modbot/modbot.py
+++ modbot/modbot.py
@@ -3,11 +3,13 @@ import sys
 import dotbot
 import yaml
 from dotbot.config import ConfigReader
+from dotbot.messenger import Messenger
 
 
 def add(config_file, filename, target=None):
     home = os.getenv('HOME')
     dotfiles = os.getenv('DOTFILES')
+    log = Messenger()
     path, filename = os.path.split(filename)
 
     if not path:
@@ -35,6 +37,7 @@ def add(config_file, filename, target=None):
     with open(config_file, 'w') as f:
         yaml.safe_dump(config, f, default_flow_style=False)
 
+    log.info('Moving {0} from {1} to {2}'.format(filename, path, target_path))
     os.rename(filename, target)
     sys.argv[1:] = ['--config-file', config_file]
     dotbot.main()
