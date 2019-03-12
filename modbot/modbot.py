e39942bc3526db2c90771f41b222cd722f808029
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-11 20:50:53 -0700

    Keep home folder in variable used for file transfer

--- modbot/modbot.py
+++ modbot/modbot.py
@@ -27,24 +27,24 @@ def add(config_file, filename, target=None):
     if not config_path:
         config_path = dotfiles
 
-    filename = os.path.join(path.replace(home, '~'), filename)
+    fullfile = os.path.join(path.replace(home, '~'), filename)
     target = os.path.join(target_path.replace(dotfiles, ''), target)
     config_file = os.path.join(config_path, config_file)
     config = _read_config(config_file)
     i = [i for i, d in enumerate(config) if 'link' in d][0]
 
-    if filename in config[i]['link']:
+    if fullfile in config[i]['link']:
         log.error('File already in config')
         exit(1)
 
-    config[i]['link'][filename] = target
+    config[i]['link'][fullfile] = target
 
     with open(config_file, 'w') as f:
         yaml.safe_dump(config, f, default_flow_style=False)
 
     if not os.path.isfile(target):
         log.info('Moving {0} from {1} to {2}'.format(filename, path, target_path))
-        os.rename(filename, target)
+        os.rename(os.path.join(path, filename), target)
         sys.argv[1:] = ['--config-file', config_file]
         dotbot.main()
     else:
