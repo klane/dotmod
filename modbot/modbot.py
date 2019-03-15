484b73fbc19b2070d0b4c98277e6cb181322e3b9
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-15 12:22:53 -0700

    Move configuration code to separate class

--- modbot/modbot.py
+++ modbot/modbot.py
@@ -10,7 +10,34 @@ DOTFILES = os.getenv('DOTFILES')
 LOG = Messenger()
 
 
+class Config(list):
+    def __init__(self, config_file):
+        config_path, config_file = os.path.split(config_file)
+
+        if not config_path:
+            config_path = DOTFILES
+
+        self.path = config_path
+        self.file = os.path.join(self.path, config_file)
+        reader = ConfigReader(self.file)
+        super(Config, self).__init__(reader.get_config())
+        index = [i for i, d in enumerate(self) if 'link' in d][0]
+        self.links = self[index]['link']
+
+    def add_link(self, key, value):
+        if key in self.links:
+            LOG.error('File already in config')
+            exit(1)
+
+        self.links[key] = value.replace(DOTFILES + os.sep, '')
+
+    def save(self):
+        with open(self.file, 'w') as f:
+            yaml.safe_dump(list(self), f, default_flow_style=False)
+
+
 def add(config_file, filename, target=None):
+    config = Config(config_file)
     path, filename = os.path.split(filename)
 
     if not path:
@@ -20,25 +47,14 @@ def add(config_file, filename, target=None):
         target = filename
 
     target_path, target = os.path.split(target)
-    config_path, config_file = os.path.split(config_file)
-
-    if not config_path:
-        config_path = DOTFILES
 
     if not target_path:
-        target_path = config_path
-    elif config_path not in target_path:
-        target_path = os.path.join(config_path, target_path)
+        target_path = config.path
+    elif config.path not in target_path:
+        target_path = os.path.join(config.path, target_path)
 
     fullfile = os.path.join(path.replace(HOME, '~'), filename)
     target = os.path.join(target_path, target)
-    config_file = os.path.join(config_path, config_file)
-    config = _read_config(config_file)
-    i = [i for i, d in enumerate(config) if 'link' in d][0]
-
-    if fullfile in config[i]['link']:
-        LOG.error('File already in config')
-        exit(1)
 
     if not os.path.isfile(os.path.join(path, filename)):
         LOG.error('File does not exist')
@@ -48,17 +64,10 @@ def add(config_file, filename, target=None):
         LOG.error('File already linked')
         exit(1)
 
-    config[i]['link'][fullfile] = target.replace(DOTFILES + os.sep, '')
-
-    with open(config_file, 'w') as f:
-        yaml.safe_dump(config, f, default_flow_style=False)
+    config.add_link(fullfile, target)
+    config.save()
 
     LOG.info('Moving {0} from {1} to {2}'.format(filename, path, target_path))
     os.rename(os.path.join(path, filename), target)
-    sys.argv[1:] = ['--config-file', config_file]
+    sys.argv[1:] = ['--config-file', config.file]
     dotbot.main()
-
-
-def _read_config(config_file):
-    reader = ConfigReader(config_file)
-    return reader.get_config()
