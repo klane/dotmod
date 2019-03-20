a98df5073f12ecbea06059d6bcb7391c418b6be2
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-19 19:03:02 -0700

    Remove list as config superclass

--- modbot/config.py
+++ modbot/config.py
@@ -6,7 +6,7 @@ from dotbot.config import ConfigReader
 from modbot import DOTFILES, LOG
 
 
-class Config(list):
+class Config(object):
     def __init__(self, config_file):
         config_path, config_file = os.path.split(config_file)
 
@@ -16,9 +16,9 @@ class Config(list):
         self.path = config_path
         self.file = os.path.join(self.path, config_file)
         reader = ConfigReader(self.file)
-        super(Config, self).__init__(reader.get_config())
-        index = [i for i, d in enumerate(self) if 'link' in d][0]
-        self.links = self[index]['link']
+        self.config = reader.get_config()
+        index = [i for i, d in enumerate(self.config) if 'link' in d][0]
+        self.links = self.config[index]['link']
 
     def add_link(self, key, value):
         if key in self.links:
@@ -38,4 +38,4 @@ class Config(list):
 
     def save(self):
         with open(self.file, 'w') as f:
-            yaml.safe_dump(list(self), f, default_flow_style=False)
+            yaml.safe_dump(self.config, f, default_flow_style=False)
