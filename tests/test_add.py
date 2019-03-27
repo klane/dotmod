5c1f5cb62b66f738798ce285e434e5c976c1c5d6
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-26 21:01:27 -0700

    Rename pytest fixture

--- tests/test_add.py
+++ tests/test_add.py
@@ -12,7 +12,7 @@ target = os.path.join(os.getcwd(), filename)
 
 
 @pytest.fixture()
-def modbot_mock(mocker):
+def mocked_modbot(mocker):
     config = mocker.MagicMock()
     config.path = os.path.join(HOME, 'project', 'modbot')
     config.file = os.path.join(config.path, 'install.conf.yaml')
@@ -25,11 +25,11 @@ def modbot_mock(mocker):
     return mocks(config, dotbot, rename)
 
 
-def test_add(modbot_mock):
-    config = modbot_mock.config
+def test_add(mocked_modbot):
+    config = mocked_modbot.config
     add(config, source)
 
     config.add_link.assert_called_once_with(source.replace(HOME, '~'), target)
     config.save.assert_called_once()
-    modbot_mock.dotbot.assert_called_once_with(config.file)
-    modbot_mock.rename.assert_called_once_with(source, target)
+    mocked_modbot.dotbot.assert_called_once_with(config.file)
+    mocked_modbot.rename.assert_called_once_with(source, target)
