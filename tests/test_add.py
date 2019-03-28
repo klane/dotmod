dcff953f7e262ef49a7de2222332fa944981c2fd
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-27 21:02:13 -0700

    Parametrize add test

--- tests/test_add.py
+++ tests/test_add.py
@@ -8,8 +8,6 @@ from modbot.modbot import add
 
 DOTFILES = os.path.join(HOME, 'dotfiles')
 filename = '.testfile'
-source = os.path.join(HOME, filename)
-target = os.path.join(DOTFILES, filename)
 
 
 @pytest.fixture()
@@ -20,17 +18,53 @@ def mocked_modbot(mocker):
 
     dotbot = mocker.patch('modbot.modbot.run_dotbot')
     rename = mocker.patch('os.rename')
-    mocker.patch('os.path.isfile', lambda file: file == source)
 
     mocks = namedtuple('mocks', 'config dotbot rename')
     return mocks(config, dotbot, rename)
 
 
-def test_add(mocked_modbot):
+@pytest.mark.parametrize('source_path, source_file, target_path, target_file', [
+    (None, filename, None, None),
+    (None, filename, None, filename),
+    (None, filename, None, filename + '1'),
+    (None, filename, DOTFILES, filename),
+    (None, filename, DOTFILES, filename + '1'),
+    (HOME, filename, None, None),
+    (HOME, filename, None, filename),
+    (HOME, filename, None, filename + '1'),
+    (HOME, filename, DOTFILES, filename),
+    (HOME, filename, DOTFILES, filename + '1')
+])
+def test_add(source_path, source_file, target_path, target_file, mocker, mocked_modbot):
     config = mocked_modbot.config
-    add(config, source)
 
-    config.add_link.assert_called_once_with(source.replace(HOME, '~'), target)
+    if source_path is None:
+        source_path = os.getcwd()
+        source = source_file
+    else:
+        source = os.path.join(source_path, source_file)
+
+    if target_path is None:
+        target_path = config.path
+        target = target_file
+    else:
+        target = os.path.join(target_path, target_file)
+
+    xsource = os.path.join(source_path, source_file)
+
+    if target_file is None:
+        xtarget = os.path.join(target_path, source_file)
+    else:
+        xtarget = os.path.join(target_path, target_file)
+
+    mocker.patch('os.path.isfile', lambda file: file == xsource)
+
+    if target is None:
+        add(config, source)
+    else:
+        add(config, source, target)
+
+    config.add_link.assert_called_once_with(xsource.replace(HOME, '~'), xtarget)
     config.save.assert_called_once()
     mocked_modbot.dotbot.assert_called_once_with(config.file)
-    mocked_modbot.rename.assert_called_once_with(source, target)
+    mocked_modbot.rename.assert_called_once_with(xsource, xtarget)
