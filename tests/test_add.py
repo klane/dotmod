60b2dd061fb200133caefb4f4709f8b709b21eca
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-27 21:30:10 -0700

    Simplify add tests

--- tests/test_add.py
+++ tests/test_add.py
@@ -23,7 +23,7 @@ def mocked_modbot(mocker):
     return mocks(config, dotbot, rename)
 
 
-@pytest.mark.parametrize('source_path, source_file, target_path, target_file', [
+@pytest.mark.parametrize('source_path, source, target_path, target', [
     (None, file, None, None),
     (None, file, None, file),
     (None, file, None, file + '1'),
@@ -35,34 +35,22 @@ def mocked_modbot(mocker):
     (HOME, file, DOTFILES, file),
     (HOME, file, DOTFILES, file + '1')
 ])
-def test_add(source_path, source_file, target_path, target_file, mocker, mocked_modbot):
+def test_add(source_path, source, target_path, target, mocker, mocked_modbot):
     config = mocked_modbot.config
-
-    if source_path is None:
-        source_path = os.getcwd()
-        source = source_file
-    else:
-        source = os.path.join(source_path, source_file)
-
-    if target_path is None:
-        target_path = config.path
-        target = target_file
-    else:
-        target = os.path.join(target_path, target_file)
-
-    xsource = os.path.join(source_path, source_file)
-
-    if target_file is None:
-        xtarget = os.path.join(target_path, source_file)
-    else:
-        xtarget = os.path.join(target_path, target_file)
-
+    xsource = os.path.join(source_path or os.getcwd(), source)
+    xtarget = os.path.join(target_path or config.path, target or source)
     mocker.patch('os.path.isfile', lambda f: f == xsource)
 
-    if target is None:
-        add(config, source)
-    else:
-        add(config, source, target)
+    if source_path is not None:
+        source = os.path.join(source_path, source)
+
+    if target_path is not None:
+        target = os.path.join(target_path, target)
+
+    if config.path not in xtarget:
+        xtarget = os.path.join(config.path, xtarget)
+
+    add(config, source, target)
 
     config.add_link.assert_called_once_with(xsource.replace(HOME, '~'), xtarget)
     config.save.assert_called_once()
