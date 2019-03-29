b93a5d512ca2523ce431ece5eea795522365d586
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-29 14:03:15 -0700

    Set DOTFILES to current directory if environment variable not set

--- modbot/__init__.py
+++ modbot/__init__.py
@@ -3,5 +3,5 @@ import os
 from dotbot.messenger import Messenger
 
 HOME = os.getenv('HOME') or os.getenv('USERPROFILE')
-DOTFILES = os.getenv('DOTFILES')
+DOTFILES = os.getenv('DOTFILES') or os.getcwd()
 LOG = Messenger()
