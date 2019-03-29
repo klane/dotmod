ea5bbd2c8fb0bfc7209009c531d4d0fdaecbf199
Kevin Lane <lane.kevin.a@gmail.com>
2019-03-29 09:18:59 -0700

    Add exception testing for add function

--- tests/test_add.py
+++ tests/test_add.py
@@ -7,6 +7,12 @@ options = [(None, HOME), [file], (None, DOTFILES, 'test'), (None, file, file + '
 options = [o for o in product(*options) if o[2] is None or o[3] is not None]
 
 
+@pytest.fixture()
+def mocked_isfile(mocked_modbot, mocker):
+    mocker.patch('os.path.isfile')
+    return mocked_modbot
+
+
 @pytest.mark.parametrize('source_path, source, target_path, target', options)
 def test_add(source_path, source, target_path, target, mocked_modbot, mocker):
     config = mocked_modbot.config
@@ -29,3 +35,12 @@ def test_add(source_path, source, target_path, target, mocked_modbot, mocker):
     config.save.assert_called_once()
     mocked_modbot.rename.assert_called_once_with(xsource, xtarget)
     mocked_modbot.dotbot.assert_called_once_with(config.file)
+
+
+@pytest.mark.parametrize('mocks, message', [
+    (pytest.lazy_fixture('mocked_modbot'), 'does not exist'),
+    (pytest.lazy_fixture('mocked_isfile'), 'already linked')
+])
+def test_exceptions(mocks, message):
+    with pytest.raises(OSError, match=message):
+        add(mocks.config, file)
