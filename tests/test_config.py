630e8bdf7b507ade23926081997b3427f43a7af0
Kevin Lane <lane.kevin.a@gmail.com>
2019-04-04 22:02:10 -0700

    Add test for Config save method

--- tests/test_config.py
+++ tests/test_config.py
@@ -42,3 +42,19 @@ def config(config_file, mocker, request):
 
 def test_config(config, config_file):
     assert all([a == b for a, b in zip(config.config, yaml.safe_load(config_file))])
+
+
+def test_save(config, config_file, mocker):
+    mock_open = mocker.mock_open()
+    mock_yaml = mocker.patch('yaml.safe_dump')
+
+    try:
+        mock_open = mocker.patch('__builtin__.open', mock_open)
+    except ImportError:
+        mock_open = mocker.patch('builtins.open', mock_open)
+
+    config.save()
+
+    mock_open.assert_called_once_with(config.file, 'w')
+    mock_file = mock_open(config.file, 'w')
+    mock_yaml.assert_called_once_with(yaml.safe_load(config_file), mock_file, default_flow_style=False)
