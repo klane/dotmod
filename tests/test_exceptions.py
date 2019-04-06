e9be700637fe78f3b50f21184e299a11e459ccb4
Kevin Lane <lane.kevin.a@gmail.com>
2019-04-05 19:29:43 -0700

    Add test for Config exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -4,6 +4,7 @@ import pytest
 
 from modbot import DOTFILES
 from modbot.modbot import add, remove
+from modbot.config import ConfigError
 from tests import file
 
 
@@ -16,3 +17,15 @@ from tests import file
 def test_modbot_exceptions(function, mocks, message):
     with pytest.raises(OSError, match=message):
         function(mocks.config, os.path.join(DOTFILES, file))
+
+
+@pytest.mark.parametrize('name, inputs, message', [
+    ('add_link', ['~/' + file, os.path.join(DOTFILES, file)], 'already in config'),
+    ('remove_link', [os.path.join(DOTFILES, '.fakefile')], 'not in config')
+])
+def test_config_exceptions(mock_config, name, inputs, message):
+    config = mock_config.config
+
+    with pytest.raises(ConfigError, match=message):
+        method = getattr(config, name)
+        method(*inputs)
