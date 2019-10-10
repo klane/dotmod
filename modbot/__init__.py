52b84d27943b4a7fc0f2deffb43e3c651b641837
Kevin Lane <lane.kevin.a@gmail.com>
2019-10-10 16:29:46 -0700

    Ensure DOTFILES does not have a trailing slash

--- modbot/__init__.py
+++ modbot/__init__.py
@@ -3,5 +3,5 @@ import os
 from dotbot.messenger import Messenger
 
 HOME = os.getenv('HOME') or os.getenv('USERPROFILE')
-DOTFILES = os.getenv('DOTFILES') or os.getcwd()
+DOTFILES = (os.getenv('DOTFILES') or os.getcwd()).rstrip(os.sep)
 LOG = Messenger()
